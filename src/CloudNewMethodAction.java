7e06ec7749af16e72b88d336cd5cfcde261c1053
zhoujun5 <zhoujun5@jd.com>
2019-12-20 21:43:59 +0800

    提交配置

--- src/CloudNewMethodAction.java
+++ src/CloudNewMethodAction.java
@@ -1,8 +1,9 @@
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.editor.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.ui.popup.ListPopup;
 
 public class CloudNewMethodAction extends AnAction {
 
@@ -11,6 +12,23 @@ public class CloudNewMethodAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
         project = e.getData(PlatformDataKeys.PROJECT);
-        Messages.showErrorDialog(project,"create mvp code success","title");
+        Editor editor=e.getData(PlatformDataKeys.EDITOR);
+        CaretModel caretModel=editor.getCaretModel();
+        LogicalPosition  logicalPosition=caretModel.getLogicalPosition();
+        VisualPosition visualPosition=caretModel.getVisualPosition();
+        Caret caret=caretModel.getCaretAt(visualPosition);
+      /*  DefaultActionGroup actionGroup = (DefaultActionGroup) ActionManager.getInstance().getAction("Sample_JBPopupActionGroup");
+        actionGroup.removeAll();
+        actionGroup.add(new AnAction("Sample Action") {
+            @Override
+            public void actionPerformed(AnActionEvent e) {
+                CreateMethodDialog createMethodDialog=new CreateMethodDialog();
+                createMethodDialog.pack();
+                createMethodDialog.setVisible(true);
+            }
+        });
+        ListPopup listPopup = JBPopupFactory.getInstance().createActionGroupPopup("ListPopup Sample", actionGroup, e.getDataContext(), JBPopupFactory.ActionSelectionAid.SPEEDSEARCH, false);
+        listPopup.showInBestPositionFor(e.getDataContext());
+*/
     }
 }
---
+++ src/CreateMethodDialog.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.Vector;
+
+public class CreateMethodDialog extends JDialog {
+    private JPanel contentPane;
+    private JList list1;
+
+    public CreateMethodDialog() {
+        setContentPane(contentPane);
+        setModal(true);
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        Vector<Integer> list=new Vector<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list1.setListData(list);
+        list1.setVisible(true);
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        CreateMethodDialog dialog = new CreateMethodDialog();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
